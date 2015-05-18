174d464107167a4ad2339ae31657718dc5fa73b2
lashevskiy <lashevskiy@gmail.com>
2015-05-18 23:30:18 +0300

    fixed bugs in path to files

--- php/tickets.php
+++ php/tickets.php
@@ -329,6 +329,16 @@ echo<<<_END
 			<div class="fon">								
 				<div class="found_form parentbox">	
 					<form method="POST" action="tickets.php">
+						<div class="info_tickets info">
+							<h2>Авиабилеты</h2>
+							<h3>На этой странице вы можете:</h3>
+							<ul>								
+								<li>Найти билеты в нужном направлении на определенную дату. Для этого задайте направление и дату (по умолчанию стоит сегодня).</li>
+								<li>Сравнить и выбрать наиболее подходящий класс перелета по стоимоси билетов.</li>								
+								<li>Забронировать понравившиеся билеты.</li>
+							</ul>
+						</div>
+						<hr class="hr_search_tickets">
 						<div class="radio_choise">
 							<div class="inline_block">								
 								<input type="radio" name="direction" value="2" id="two" $checked_2><span><label for="two">Туда и обратно</label></span>
@@ -340,7 +350,7 @@ echo<<<_END
 								<a href="#">Составной</a>
 							</div>
 						</div>
-						<hr class="hr_search_tickets">	
+							
 						<div class="inline_block owerflow_hide">
 							<h3>Город вылета</h3>
 							<input type="text" class="input width_1" name="flight_from[0]" list="flight_from" id="flight_from_id" placeholder="Откуда?"  value = "$flight_from[0]" autocomplete="off" onkeyup='checkValue()' onkeypress='showResult(this)' required>							
@@ -416,9 +426,9 @@ _END;
 
 						//if($direction == "one")
 						//{
-						$two = false;
-						$one = false;
-						$flag = false;
+						
+						
+						
 					
 						
 						
@@ -426,123 +436,106 @@ _END;
 						$result = array(2);
 						$num = array(2);
 
-						if($direction == "2")
-						{					
-							$query[0]  = "SELECT DISTINCT schedule.flight_date, flights.flight_number, flights.flight_from, flights.flight_to, flights.flight_from_code, flights.flight_to_code, IF(schedule.time_up IS NULL, flights.time_up, schedule.time_up) AS time_up, IF(schedule.time_down IS NULL, flights.time_down, schedule.time_down) AS time_down, flights.time_flight, flights.airplane_type, IF(schedule.price_economy IS NULL, flights.price_economy, schedule.price_economy) AS price_economy, IF(schedule.price_standard IS NULL, flights.price_standard, schedule.price_standard) AS price_standard, IF(schedule.price_business IS NULL, flights.price_business, schedule.price_business) AS price_business FROM flights, schedule WHERE flights.flight_number = schedule.flight_number AND flight_from = '$flight_from[0]' AND flight_to = '$flight_to[0]' AND schedule.flight_date = '$flight_date[0]' ORDER BY time_up";
-							$result[0] = queryMysql($query[0]);
-	    					$num[0]    = $result[0]->num_rows;
-							
-							$query[1]  = "SELECT DISTINCT schedule.flight_date, flights.flight_number, flights.flight_from, flights.flight_to, flights.flight_from_code, flights.flight_to_code, IF(schedule.time_up IS NULL, flights.time_up, schedule.time_up) AS time_up, IF(schedule.time_down IS NULL, flights.time_down, schedule.time_down) AS time_down, flights.time_flight, flights.airplane_type, IF(schedule.price_economy IS NULL, flights.price_economy, schedule.price_economy) AS price_economy, IF(schedule.price_standard IS NULL, flights.price_standard, schedule.price_standard) AS price_standard, IF(schedule.price_business IS NULL, flights.price_business, schedule.price_business) AS price_business FROM flights, schedule WHERE flights.flight_number = schedule.flight_number AND flight_from = '$flight_to[0]' AND flight_to = '$flight_from[0]' AND schedule.flight_date = '$flight_date[1]' ORDER BY time_up";
-							$result[1] = queryMysql($query[1]);
-	    					$num[1]    = $result[1]->num_rows;
 
-	    					if($num[0] == 0 || $num[1] == 0)
-	    					{
-    							echo '
-    								<div class="found_form center">Билетов не найдено. Попробуйте выбрать другой маршрут.</div>
-    							';	    						
-	    					}
-	    					else 
-	    					{
-	    						$two = true; 
-	    						$one = true;
-	    						$flag = true;
-	    					}
-						}
-						else if($direction == "1")
+						date_default_timezone_set("Europe/Helsinki");
+    					$date = date("Y-m-d");
+    					$time = date("H:i:s");
+
+    					$flag = true;
+
+						if($direction == "1")
 						{						
-							$query[0]  = "SELECT DISTINCT schedule.flight_date, flights.flight_number, flights.flight_from, flights.flight_to, flights.flight_from_code, flights.flight_to_code, IF(schedule.time_up IS NULL, flights.time_up, schedule.time_up) AS time_up, IF(schedule.time_down IS NULL, flights.time_down, schedule.time_down) AS time_down, flights.time_flight, flights.airplane_type, IF(schedule.price_economy IS NULL, flights.price_economy, schedule.price_economy) AS price_economy, IF(schedule.price_standard IS NULL, flights.price_standard, schedule.price_standard) AS price_standard, IF(schedule.price_business IS NULL, flights.price_business, schedule.price_business) AS price_business FROM flights, schedule WHERE flights.flight_number = schedule.flight_number AND flight_from = '$flight_from[0]' AND flight_to = '$flight_to[0]' AND schedule.flight_date = '$flight_date[0]' ORDER BY time_up";
+							$query[0]  = "SELECT DISTINCT schedule.flight_date, flights.flight_number, flights.flight_from, flights.flight_to, flights.flight_from_code, flights.flight_to_code, IF(schedule.time_up IS NULL, flights.time_up, schedule.time_up) AS time_up, IF(schedule.time_down IS NULL, flights.time_down, schedule.time_down) AS time_down, flights.time_flight, flights.airplane_type, IF(schedule.price_economy IS NULL, flights.price_economy, schedule.price_economy) AS price_economy, IF(schedule.price_standard IS NULL, flights.price_standard, schedule.price_standard) AS price_standard, IF(schedule.price_business IS NULL, flights.price_business, schedule.price_business) AS price_business FROM flights, schedule WHERE flights.flight_number = schedule.flight_number AND flight_from = '$flight_from[0]' AND flight_to = '$flight_to[0]' AND schedule.flight_date = '$flight_date[0]' AND IF($flight_date[0] = $date, IF(schedule.time_up IS NULL, flights.time_up, schedule.time_up) > '$time', true) ORDER BY time_up";
 							$result[0] = queryMysql($query[0]);
 	    					$num[0]    = $result[0]->num_rows;				
-
 	    					
 
 	    					if($num[0] == 0)
 	    					{
-    							echo '
-    								<div class="found_form center">Билетов не найдено. Попробуйте выбрать другой маршрут.</div>
-    							';	    						
-	    					}
-	    					else 
-	    					{	    						
-	    						$one = true;
-	    						$flag = true;
-	    					}
+	    						$flag = false;
+	    					}				
+	    					
 						}
-					
-						echo '<form method="POST" action="buy_tickets.php">';
-						for ($i=0; $i < $direction; $i++) 					
-    					{
-    						if($i!=0)
+						if($direction == "2")
+						{					
+							$query[0]  = "SELECT DISTINCT schedule.flight_date, flights.flight_number, flights.flight_from, flights.flight_to, flights.flight_from_code, flights.flight_to_code, IF(schedule.time_up IS NULL, flights.time_up, schedule.time_up) AS time_up, IF(schedule.time_down IS NULL, flights.time_down, schedule.time_down) AS time_down, flights.time_flight, flights.airplane_type, IF(schedule.price_economy IS NULL, flights.price_economy, schedule.price_economy) AS price_economy, IF(schedule.price_standard IS NULL, flights.price_standard, schedule.price_standard) AS price_standard, IF(schedule.price_business IS NULL, flights.price_business, schedule.price_business) AS price_business FROM flights, schedule WHERE flights.flight_number = schedule.flight_number AND flight_from = '$flight_from[0]' AND flight_to = '$flight_to[0]' AND schedule.flight_date = '$flight_date[0]' AND IF($flight_date[0] = $date, IF(schedule.time_up IS NULL, flights.time_up, schedule.time_up) > '$time', true) ORDER BY time_up";
+							$result[0] = queryMysql($query[0]);
+	    					$num[0]    = $result[0]->num_rows;	
+							
+							$query[1]  = "SELECT DISTINCT schedule.flight_date, flights.flight_number, flights.flight_from, flights.flight_to, flights.flight_from_code, flights.flight_to_code, IF(schedule.time_up IS NULL, flights.time_up, schedule.time_up) AS time_up, IF(schedule.time_down IS NULL, flights.time_down, schedule.time_down) AS time_down, flights.time_flight, flights.airplane_type, IF(schedule.price_economy IS NULL, flights.price_economy, schedule.price_economy) AS price_economy, IF(schedule.price_standard IS NULL, flights.price_standard, schedule.price_standard) AS price_standard, IF(schedule.price_business IS NULL, flights.price_business, schedule.price_business) AS price_business FROM flights, schedule WHERE flights.flight_number = schedule.flight_number AND flight_from = '$flight_to[0]' AND flight_to = '$flight_from[0]' AND schedule.flight_date = '$flight_date[1]' AND IF($flight_date[1] = $date, IF(schedule.time_up IS NULL, flights.time_up, schedule.time_up) > '$time', true) ORDER BY time_up";
+							$result[1] = queryMysql($query[1]);
+	    					$num[1]    = $result[1]->num_rows;
+
+	    					if($num[0] == 0 or $num[1]==0)
+	    					{
+	    						$flag = false;    							    						
+	    					}				
+	    				
+						}
+
+						if($flag == true)
+						{    					
+							echo '<form method="POST" action="buy_tickets.php">';
+							for ($i=0; $i < $direction; $i++) 					
     						{
-    							$flight_from_str = $flight_to[0];
-    							$flight_to_str = $flight_from[0];
-    						}
-    						else
-    						{
-    							$flight_from_str = $flight_from[0];
-    							$flight_to_str = $flight_to[0];
-    						}
-							echo <<< _END
-							<div class="select_flights ">	
-								<div>
-									<h3>Выберите рейс: $flight_from_str - $flight_to_str</h3>								
-								</div>
-							</div>	
-							<table class="" id="$i">
-								<tr>
-									<th colspan="2">Вылет</th>
-					    	   		<th>Прилет</th>
-					    	   		<th>Рейс</th>
-					    	   		<th>В пути</th>
-					    	   		<th>Эконом</th>
-					    	   		<th>Стандарт</th>
-					    	   		<th>Бизнес</th>
-				    			</tr>	
+    							if($i!=0)
+    							{
+    								$flight_from_str = $flight_to[0];
+    								$flight_to_str = $flight_from[0];
+    							}
+    							else
+    							{
+    								$flight_from_str = $flight_from[0];
+    								$flight_to_str = $flight_to[0];
+    							}
+								echo <<< _END
+								<div class="select_flights ">	
+									<div>
+										<h3>Выберите рейс: $flight_from_str - $flight_to_str</h3>								
+									</div>
+								</div>	
+								<table class="" id="$i">
+									<tr>
+										<th colspan="2">Вылет</th>
+					    		   		<th>Прилет</th>
+					    		   		<th>Рейс</th>
+					    		   		<th>В пути</th>
+					    		   		<th>Эконом</th>
+					    		   		<th>Стандарт</th>
+					    		   		<th>Бизнес</th>
+				    				</tr>	
 _END;
     					 		
-    					 	date_default_timezone_set("Europe/Helsinki");
-    					 	$date = date("Y-m-d");
+    					 	
 
-       						for ($j = 0 ; $j < $num[$i] ; ++$j)
-    						{
-      							$row = $result[$i]->fetch_array(MYSQLI_ASSOC);      						
-      							$flight_number[$j] = $row['flight_number'];      						
-      							$time_up = $row['time_up'];
-      							$time_down = $row['time_down'];
-								$time_flight = $row['time_flight'];
-								$price_economy = $row['price_economy'];
-								$price_standard = $row['price_standard'];
-								$price_business = $row['price_business']; 	
-      						
-	      						$time_down_obj = new DateTime($time_down);
-								$time_up_obj = new DateTime($time_up);
-								$time_flight = new DateTime($time_flight);		
+       							for ($j = 0 ; $j < $num[$i] ; ++$j)
+    							{
+      								$row = $result[$i]->fetch_array(MYSQLI_ASSOC);      						
+      								$flight_number[$j] = $row['flight_number'];      						
+      								$time_up = $row['time_up'];
+      								$time_down = $row['time_down'];
+									$time_flight = $row['time_flight'];
+									$price_economy = $row['price_economy'];
+									$price_standard = $row['price_standard'];
+									$price_business = $row['price_business']; 	
+      							
+	      							$time_down_obj = new DateTime($time_down);
+									$time_up_obj = new DateTime($time_up);
+									$time_flight = new DateTime($time_flight);		
+										
 									
-								
-								if($time_flight->format('G') == '0')
-									$time_flight = $time_flight->format('iм');	
-								else $time_flight = $time_flight->format('Gч iм');
-								
+									if($time_flight->format('G') == '0')
+										$time_flight = $time_flight->format('iм');	
+									else $time_flight = $time_flight->format('Gч iм');
 
-								$time = date("H:i:s");																	
-												
-
-								if($flight_date[$i] == $date)
-								{
 									
-									if($time < $time_up)
-									{
 									$id1 = $j * 3;
 									$id1 = $i . "_" . $id1;
-		
-									$id2 = $j * 3 + 1;
+												$id2 = $j * 3 + 1;
 									$id2 = $i . "_" . $id2;
-		
-									$id3 = $j * 3 + 2;
+												$id3 = $j * 3 + 2;
 									$id3 = $i . "_" . $id3;		
-   							
-									echo <<<_END
+   																	echo <<<_END
 	      							<tr>
 	      								<td class="active_td"><div class="active"></div></td>
 										<td>$time_up</td>
@@ -569,54 +562,14 @@ _END;
 										</td>
 									</tr>	
 _END;
-								}
-								}
-								else
-								{
-									$id1 = $j * 3;
-									$id1 = $i . "_" . $id1;
-		
-									$id2 = $j * 3 + 1;
-									$id2 = $i . "_" . $id2;
-		
-									$id3 = $j * 3 + 2;
-									$id3 = $i . "_" . $id3;		
-   							
-									echo <<<_END
-	      							<tr>
-	      								<td class="active_td"><div class="active"></div></td>
-										<td>$time_up</td>
-										<td>$time_down</td>
-										<td>$flight_number[$j]</td>
-										<td>$time_flight</td>								
-										<td class="td_price2" onclick="setColor(this)">
-											<div class="radio_buttons2">
-												<input type="radio" name="flight_number[$i]" value="$flight_number[$j]" id="$id1" required $disabled[price_economy]>										
-												<label for="$id1" ><div>$price_economy руб</div></label>
-											</div>
-										</td>
-										<td class="td_price2" onclick="setColor(this)">
-											<div class="radio_buttons2">
-												<input type="radio" name="flight_number[$i]" value="$flight_number[$j]" id="$id2" required $disabled[price_standard]>
-												<label for="$id2"><div>$price_standard руб</div></label>
-											</div>
-										</td>
-										<td class="td_price2" onclick="setColor(this)">
-											<div class="radio_buttons2">
-												<input type="radio" name="flight_number[$i]" value="$flight_number[$j]" id="$id3" required $disabled[price_business]>
-												<label for="$id3"><div>$price_business руб</div></label>
-											</div>
-										</td>
-									</tr>	
-_END;
-								}
-	      					}	
-	      					echo "</table>";		      				
-	      				}   
+									
+	      						}	
+	      						echo "</table>";		      				
+	      					}   
 	      				
 
-	      				if($flag == true)
-	      				{
+	      					
+	      					
 							echo <<<_END
 							<div>					
 								<input type="hidden" name="flight_date[0]" value="$flight_date[0]">
@@ -634,14 +587,20 @@ _END;
 								{
 									echo '<a href="login.php"><input type="button" class="button" value="Войти для бронирования"></a>';
 								}
-
-							echo <<<_END
-							</div>
-							</form>															
-							<div class="clear"></div>
+	
+								echo <<<_END
+								</div>
+								</form>															
+								<div class="clear"></div>
 _END;
+							
 						}
-					
+						else
+						{
+							echo '
+    							<div class="found_form center">На сегодня рейсов уже нет. Попробуйте выбрать другой маршрут или дату.</div>
+    						';	    											
+						}					
 					}
 					
 				?>				
